a45ae385b4627d0655e5fdf47ee46685b9ea91f6
kirill24097 <110169552+kirill24097@users.noreply.github.com>
2022-08-18 12:40:54 +0300

    курсовая1

---
+++ src/Kursovaya1/Employee.java
@@ -0,0 +1,62 @@
+package Kursovaya1;
+
+    public class Employee {
+        private static int counter;
+        private final Integer id;
+        private final String name;
+        private final String lastName;
+        private final String middleName;
+        private int department;
+        private int salary;
+
+
+
+        public Employee(String name, String lastName, String middleName, int department, int salary) {
+            this.name = name;
+            this.lastName = lastName;
+            this.middleName = middleName;
+            this.department = department;
+            this.salary = salary;
+            this.id = counter++;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public String getMiddleName() {
+            return middleName;
+        }
+
+        public int getDepartment() {
+            return department;
+        }
+
+        public Integer getId() {
+            return this.id;
+        }
+
+        public int getSalary() {
+            return salary;
+        }
+
+        public void setSalary(int salary) {
+            this.salary = salary;
+        }
+
+        public void setDepartment(byte department) {
+            this.department = department;
+        }
+
+        @Override
+        public String toString() {
+            return "Сотрудник  " + "ID =  " + "Имя=  " + name + " " + "фамилия=  " + lastName + "  " + "отчество=  " + middleName + "  " +
+                    "Отдел=  " + department + "  " + "Зарплата  " + salary + "  ";
+        }
+
+    }
+
---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import Kursovaya1.Employee;
+
+public class Main {
+
+    private static final Employee[] employees = new Employee[10];
+
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("кирилл", "Смирнов", "Анатольевич", 1, 113333);
+        Employee emp2 = new Employee("кирилл", "Смирнов", "Анатольевич", 1, 113333);
+
+        System.out.println(emp1);
+        System.out.println(emp2);
+
+        employees[0] = emp1;
+
+    }
+
+    public static void printEmployees() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+    public static int calculateTotalSalary() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            sum += employee.getSalary();
+        }
+        return sum;
+    }
+
+
+    public static Employee findEmployeeWithMinSalary() {
+        Employee result = employees[0];
+        int minSalary = employees[0].getSalary();
+        for (Employee employee : employees) {
+            if (employee.getSalary() < minSalary) {
+                minSalary = employee.getSalary();
+                result = employee;
+            }
+        }
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
